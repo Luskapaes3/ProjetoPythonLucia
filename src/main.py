602eb9624d1ade101adfd9bd59fa99c82ef12d49
VitinAM <victor3.marucci.2015@gmail.com>
2026-04-13 09:08:51 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-Zona_Vermelha = 0
-Zona_Verde = 0
-Zona_Amarela = 0
+zona_vermelha = 0
+zona_verde = 0
+ajuste = 0
+leituras_realizadas = 0
 
-while (Zona_Vermelha < 2):
-    Valor = float(input("Insira o valor em PCU"))
-    if Valor > 150:
-        Ajuste = Valor + Valor*0.08
+while (zona_vermelha < 2):
+    valor = float(input("Insira o valor em PCU"))
+    leituras_realizadas +=1
+    if valor > 150:
+        ajuste = valor + valor*0.08
+        if ajuste > 250:
+            zona_vermelha +=1
+        elif 120 < ajuste < 180:
+            zona_verde += 1
+            zona_vermelha -= 1
+        elif 180 < ajuste < 250:
+            zona_vermelha -= 1
+
     else:
-        Ajuste = Valor - Valor*0.04
+        ajuste = valor - valor*0.04
+print("Lucas da a bundinha dele")
