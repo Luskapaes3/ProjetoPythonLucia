5982c8292372470df591b42789e8eeef6644eefe
VitinAM <victor3.marucci.2015@gmail.com>
2026-04-13 09:22:31 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
-greenZone = 0
+redZone = 0
 greenZone = 0
 adjust = 0
 lecturesRealized = 0
 
-while (greenZone < 2):
-    value = float(input("Insira o valor em PCU: "))
+while (redZone < 2):
+    value = int(input("Insira o valor em PCU: "))
     lecturesRealized +=1
     if value > 150:
         adjust = value + value*0.08
         if adjust > 250:
-            greenZone +=1
-        elif 120 < adjust < 180:
+            redZone +=1
+        elif 180 < adjust:
+            redZone -= 1
+        else:
             greenZone += 1
-            greenZone -= 1
-        elif 180 < adjust < 250:
-            greenZone -= 1
+            redZone -= 1       
     else:
         adjust = value - value*0.04
+        if 120 < adjust:
+            greenZone += 1
+            redZone -= 1
 print("Lucas da a bundinha dele")
 
