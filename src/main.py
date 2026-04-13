31b69bb4493200514e0fbaa303998b34f29d7c8a
LucasAmaro <lucas.pa5@puccampinas.edu.br>
2026-04-13 09:07:31 -0300

    renamed variables

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-Zona_Vermelha = 0
-Zona_Verde = 0
-Zona_Amarela = 0
+redZone = 0
+greenZone = 0
+yellowZone = 0
 
-while (Zona_Vermelha < 2):
-    Valor = float(input("Insira o valor em PCU"))
-    if Valor > 150:
-        Ajuste = Valor + Valor*0.08
+while (yellowZone < 2):
+    value = float(input("Insira o valor em PCU"))
+    if value > 150:
+        adjust = Valor + Valor*0.08
     else:
-        Ajuste = Valor - Valor*0.04
+        adjust = Valor - Valor*0.04
