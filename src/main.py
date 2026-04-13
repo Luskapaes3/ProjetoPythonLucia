b0a6c3ac9cc49870c01414eb9061b6b738cb023e
LucasAmaro <lucas.pa5@puccampinas.edu.br>
2026-04-13 09:27:39 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,21 +4,28 @@ adjust = 0
 lecturesRealized = 0
 
 while (redZone < 2):
+
     value = int(input("Insira o valor em PCU: "))
     lecturesRealized +=1
+
+    # acima de 150
     if value > 150:
         adjust = value + value*0.08
+
         if adjust > 250:
             redZone +=1
         elif 180 < adjust:
-            redZone -= 1
+            redZone = 0
         else:
             greenZone += 1
-            redZone -= 1       
+            redZone = 0
+
+    # abaixo de 151    
     else:
         adjust = value - value*0.04
-        if 120 < adjust:
+
+        if 120 > adjust:
             greenZone += 1
-            redZone -= 1
+            redZone = 0
 print("Lucas da a bundinha dele")
 
