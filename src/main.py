d3097cd71729393623743c70673d93b94bf79c91
LucasAmaro <lucas.pa5@puccampinas.edu.br>
2026-04-27 11:24:51 -0300

    main refeita.py

--- src/main.py
+++ src/main.py
@@ -1,63 +1,52 @@
-redZone = 0
-greenZone = 0
-adjust = 0
-lecturesRealized = 0
-smallestLecture = 0
-total = 0
-conclude = 0
-
-while (conclude != 2):
-
-    value = int(input("Insira o valor em PCU: "))
-    lecturesRealized +=1
+def adjust_value(value):
     if value > 150:
-        adjust = value + value*0.08
-        total = total + adjust
-
-        if adjust > 250:
-            redZone +=1
-#script pra brecar o codigo agora ao vivo se pegar duas vezes seguidas
-            if redZone >= 2:
-                print("----- SISTEMA TRAVADO ------") 
-                break
-        elif 180 < adjust:
-            redZone = 0
-        else:
-            greenZone += 1
-            redZone = 0
-
+        return value + value*0.08
     else:
-        adjust = value - value*0.04
-        total = total + adjust
-
-        if 120 > adjust:
-            greenZone += 1
-            redZone = 0
-#script bolado pra salvar o menor valor lido 
-    if smallestLecture == 0:
-        smallestLecture = adjust
-#o if coloca o primeiro valor lido como essa variavel nova, que vai ser comparada com o resto
-    elif smallestLecture > adjust:
-        smallestLecture = adjust
-#se a variavel smallest for maior que o novo valor lido, o valor lido eh armazenado como o novo smallestLecture
-
-#sistema pra concluir ou continuar inserindo dados
-    print("----- Digite 1 para continuar a leitura -----") 
-    print("----- Digite 2 para concluir a leitura -----")
-    conclude = int(input(""))        
-
-if conclude == 2:
-    print("----- SISTEMA CONCLUIDO ------")
-
-#script ja com print pra deixar o lucas de xerequinha calculando a media total das leituras realizadas
-print(f"A media de todas as leituras realizadas ajustadas foi de {total/lecturesRealized:1.2f} UPCs")
-print(f"A menor leitura ajustada foi de {smallestLecture} UPCs")
-#script de aura pra calcular quantos % foi zona verde e ja printar direto
-print(f"O percentual de leituras na zona verde realizadas foram de {(greenZone*100)/lecturesRealized:1.2f}%")
+        return value - value*0.04
 
 
 
+def handle_consecutive_red(value,con):
+
+    if value > 250:
+        return con + 1
+    else:
+        return 0
+    
+def print_estabillity(value):
+    if 180 > value > 120:
+        print(f"Valor apos dilatacao: {value} -> Zona verde")
+    elif value > 250:
+        print(f"Valor apos dilatacao: {value} -> Zona vermelha")
+    else:
+        print(f"Valor apos dilatacao: {value} -> Zona amarela")
 
 
+
+def end_system():
+    
+    print("Sistema encerrado!")
+
+
+def main_fuction():
+
+    
+    consecutive_red = 0
+    value = float(input("Insira o valor da pressao em PCU, insira um valor negativo para encerrar o programa: "))
+    while(value >= 0):
+
+        value = adjust_value(value)
+        print_estabillity(value)
+        consecutive_red = handle_consecutive_red(value,consecutive_red)
+
+        if consecutive_red < 2:
+            value = float(input("Insira o valor da pressao em PCU, insira um valor negativo para encerrar o programa: "))
+        else:
+            break
+    end_system()
+            
+
+
+main_fuction()
 
 
