115cbfc640e6014a773f0430dce396f7c8a70e79
LucasAmaro <lucas.pa5@puccampinas.edu.br>
2026-05-04 07:52:50 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ def adjust_value(value):
     else:
         return value - value*0.04
 
-
-
 def handle_consecutive_red(value,con):
 
     if value > 250:
@@ -13,14 +11,13 @@ def handle_consecutive_red(value,con):
     else:
         return 0
     
-def print_estabillity(value):
+def get_stabillity_zone(value):
     if 180 > value > 120:
-        print(f"Valor apos dilatacao: {value} -> Zona verde")
+        return "Zona verde"
     elif value > 250:
-        print(f"Valor apos dilatacao: {value} -> Zona vermelha")
+        return "Zona vermelha"
     else:
-        print(f"Valor apos dilatacao: {value} -> Zona amarela")
-
+        return "Zona amarela"
 
 
 def end_system():
@@ -28,25 +25,30 @@ def end_system():
     print("Sistema encerrado!")
 
 
+
+
+
+
+
 def main_fuction():
 
     
     consecutive_red = 0
-    value = float(input("Insira o valor da pressao em PCU, insira um valor negativo para encerrar o programa: "))
+    value = float(input(f"\nInsira o valor da pressao em PCU, insira um valor negativo para encerrar o programa: "))
     while(value >= 0):
 
         value = adjust_value(value)
-        print_estabillity(value)
+        print(f"valor  convertido: {value}")
+
+        print(f"O valor: {value} faz parte da --> {get_stabillity_zone(value)}")
+
         consecutive_red = handle_consecutive_red(value,consecutive_red)
 
         if consecutive_red < 2:
             value = float(input("Insira o valor da pressao em PCU, insira um valor negativo para encerrar o programa: "))
         else:
             break
+        
     end_system()
             
-
-
 main_fuction()
-
-
